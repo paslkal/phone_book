5717b4aa5ceefe293fb67ff8703de773e66e6341
paslkal <164247200+paslkal@users.noreply.github.com>
2025-04-24 22:04:19 +0300

    move class PhoneBook

---
+++ phone_book.py
@@ -0,0 +1,78 @@
+class PhoneBook:
+    def create(
+        self,
+        *,
+        first_name: str,
+        second_name: str,
+        last_name: str,
+        personal_phone_number: str,
+        work_phone_number: str,
+        organization: str,
+    ):
+        """добавляет новую информацию в конец файла"""
+
+        with open("phone_book.txt", "a+", encoding="utf-8") as f:
+            f.write(
+                f"{first_name} {last_name} {second_name} {work_phone_number} {personal_phone_number} {organization}\n"
+            )
+
+    def edit():
+        pass
+
+    def find(
+        self,
+        personal_phone_number: str,
+    ) -> str:
+        """находит человека по номеру телефона"""
+
+        phone_to_person = self.get_data()
+        if personal_phone_number is not None:
+            if personal_phone_number in phone_to_person:
+                return self.from_dict_to_string(phone_to_person[personal_phone_number])
+
+        return "Not found"
+
+    def get_data(self) -> dict[str, dict]:
+        """
+        читает данные в файле и возвращает в виде словаря,
+        в котором ключ -- номер телефона, а значение -- словарь со всеми данными о человеке
+        """
+
+        with open("phone_book.txt", "r", encoding="utf-8") as f:
+            string_people = f.readlines()
+            dict_people = {}
+            for string_person in string_people:
+                if string_people == " ":
+                    continue
+                person = self.from_string_to_dict(string_person)
+                dict_people[person["personal_phone_number"]] = person
+
+            return dict_people
+
+    def from_string_to_dict(self, string: str):
+        """превращает строку с данными в словарь"""
+
+        (
+            first_name,
+            second_name,
+            last_name,
+            work_phone_number,
+            personal_phone_number,
+            organization,
+        ) = string.split()
+
+        person = {
+            "first_name": first_name,
+            "second_name": second_name,
+            "last_name": last_name,
+            "work_phone_number": work_phone_number,
+            "personal_phone_number": personal_phone_number,
+            "organization": organization,
+        }
+
+        return person
+
+    def from_dict_to_string(self, person: dict):
+        """преобразует словарь с данными о человеке в строку"""
+
+        return f"{person['first_name']} {person['second_name']} {person['last_name']} {person['work_phone_number']} {person['personal_phone_number']} {person['organization']}"
